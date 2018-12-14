b4b95dc6a822cbbb337042d63bf866d7fcdf6b93
Nemanja Zoric <nemanja.zoric1.nz@gmail.com>
2018-12-14 11:43:03 +0100

    Some testing

--- src/calendar/calendar.ts
+++ src/calendar/calendar.ts
@@ -27,7 +27,7 @@ import * as _ from "lodash";
             *ngFor="let day of week;let j = index"
             [ngClass]="[day.isThisMonth?'this-month':'not-this-month',day.isToday?'today':'',day.isSelect?'select':'']">
                 {{day.date}}
-                <span class="eventBlip" *ngIf="day.hasEvent"></span>
+                <span class="eventBlip" *ngIf="day.hasEvent">1</span>
             </ion-col>
         </ion-row>
 
